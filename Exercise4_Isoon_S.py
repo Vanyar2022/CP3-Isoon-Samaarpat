ae8ce060a11a40d783a374216e25c21f464f0495
Vanyar2022 <yeppopie@gmail.com>
2022-01-30 02:44:38 +0700

    Create Exercise4_Isoon_S.py

---
+++ Exercise4_Isoon_S.py
@@ -0,0 +1,10 @@
+FE = 82.5
+GB = 85.7
+IC = 78.5
+CP = 80.0
+S  = "คะแนนที่ได้"
+print("---Your Scour---")
+print("Foundation English:",S,FE)
+print("General Business:",S,GB)
+print("Introduction to Computer System:",S,IC)
+print("Computer Programming:",S,CP)
